551825198b4edd6b9d60f48b9e95cbdc283e4829
IroshanBrian <brianstudiolk@gmail.com>
2025-02-11 14:37:04 +0530

    Add CRUD routes for subscriptions and user subscriptions

--- routes/subscription.routes.ts
+++ routes/subscription.routes.ts
@@ -8,4 +8,48 @@ subscriptionRouter.get("/", (req, res) => {
   });
 });
 
+subscriptionRouter.get("/:id", (req, res) => {
+  res.send({
+    title: "Get A Subscription",
+  });
+});
+
+subscriptionRouter.post("/", (req, res) => {
+  res.send({
+    title: "Create A Subscription",
+  });
+});
+
+subscriptionRouter.put("/:id", (req, res) => {
+  res.send({
+    title: "Update A Subscription",
+  });
+});
+
+subscriptionRouter.delete("/:id", (req, res) => {
+  res.send({
+    title: "Delete A Subscription",
+  });
+});
+
+subscriptionRouter.get("/user/:id", (req, res) => {
+  res.send({
+    title: "Get all user Subscriptions",
+  });
+});
+
+subscriptionRouter.put("/:id/cancel", (req, res) => {
+  res.send({
+    title: "Cancel A Subscription",
+  });
+});
+
+subscriptionRouter.get("/upcoming-renewals", (req, res) => {
+  res.send({
+    title: "Get Upcoming Renewals",
+  });
+});
+
+
+
 export default subscriptionRouter;
